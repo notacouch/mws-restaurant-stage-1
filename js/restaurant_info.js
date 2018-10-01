e1ca62490a071cb758e41e7557e1077094b4da31
Abed Islam <versatil@gmail.com>
2018-10-01 04:48:53 -0400

    refactor: Restaurant detail page replaced detail p tags with divs when they were not paragraphs, and list items with articles

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -157,35 +157,37 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
     container.appendChild(noReviews);
     return;
   }
-  const ul = document.getElementById('reviews-list');
+  const reviewsList = document.getElementById('reviews-list');
   reviews.forEach(review => {
-    ul.appendChild(createReviewHTML(review));
+    reviewsList.appendChild(createReviewHTML(review));
   });
-  container.appendChild(ul);
+  container.appendChild(reviewsList);
 };
 
 /**
  * Create review HTML and add it to the webpage.
  */
 createReviewHTML = review => {
-  const li = document.createElement('li');
+  const reviewNode = document.createElement('article');
+  reviewNode.className = 'reviews-list__review';
+
   const name = document.createElement('p');
   name.innerHTML = review.name;
-  li.appendChild(name);
+  reviewNode.appendChild(name);
 
   const date = document.createElement('p');
   date.innerHTML = review.date;
-  li.appendChild(date);
+  reviewNode.appendChild(date);
 
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
-  li.appendChild(rating);
+  reviewNode.appendChild(rating);
 
   const comments = document.createElement('p');
   comments.innerHTML = review.comments;
-  li.appendChild(comments);
+  reviewNode.appendChild(comments);
 
-  return li;
+  return reviewNode;
 };
 
 /**
